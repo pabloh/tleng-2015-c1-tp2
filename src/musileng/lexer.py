02ff90327e66df88d10ef9468852254e118cb4b9
Pablo Herrero <pablodherrero@gmail.com>
2015-06-24 09:57:15 -0300

    Add musileng lexer

---
+++ src/musileng/lexer.py
@@ -0,0 +1,88 @@
+keywords = {
+    'tempo'    : 'TEMPO',
+    'compas'   : 'COMPAS',
+    'const'    : 'CONST',
+    'voz'      : 'VOZ',
+    'repetir'  : 'REPETIR',
+    'nota'     : 'NOTA',
+    'silencio' : 'SILENCIO',
+}
+
+musical_notes = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si']
+note_values = ['redonda', 'blanca', 'negra', 'corchea', 'semicorchea', 'fusa', 'semifusa']
+
+
+tokens = (
+    # Operators
+    'HASH',
+    'EQUALS',
+    'SLASH',
+    'POINT',
+    'PLUS',
+    'MINUS',
+
+    # Delimiters
+    'SEMICOLON',
+    'COMMA',
+    'LPAREN',
+    'RPAREN',
+    'LBRACKET',
+    'RBRACKET',
+
+    # Misc
+    'MUSICAL_NOTE',
+    'NOTE_VALUE',
+    'NUMBER',
+    'ID',
+) + tuple(keywords.values())
+
+
+def t_NUMBER(token):
+    r'[1-9][0-9]*'
+    token.value = int(token.value)
+    return token
+
+def t_ID(token):
+    r'[_a-zA-Z][_a-zA-Z0-9]*'
+    if token.value in keywords:   # Check for keywords
+        token.type = keywords.get(token.value)
+    elif token.value in musical_notes:
+        token.type = 'MUSICAL_NOTE'
+    elif token.value in note_values:
+        token.type = 'NOTE_VALUE'
+    else:
+        token.type = 'ID'
+
+    return token
+
+
+t_HASH   = r'\#'
+t_EQUALS = r'='
+t_SLASH  = r'/'
+t_POINT  = r'\.'
+t_PLUS   = r'\+'
+t_MINUS  = r'\-'
+
+t_SEMICOLON = r';'
+t_COMMA  = r','
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\{'
+t_RBRACKET = r'\}'
+
+
+def t_NEWLINE(token):
+    r'\n+'
+    token.lexer.lineno += len(token.value)
+
+t_ignore = " \t"
+t_ignore_COMMENT = r'//[^\n]*\n'
+
+
+def t_error(token):
+    message = "Token desconocido:"
+    message += "\ntype:" + token.type
+    message += "\nvalue:" + str(token.value)
+    message += "\nline:" + str(token.lineno)
+    message += "\nposition:" + str(token.lexpos)
+    raise Exception(message)
---
+++ src/tests/lexer_test.py
@@ -0,0 +1,66 @@
+import unittest
+import musileng.lexer
+from ply import lex
+
+class TestMusilengLexer(unittest.TestCase):
+    def setUp(self):
+        self.lexer = lex.lex(module=musileng.lexer)
+
+    # Test it output
+    def tokens(self, data):
+        self.lexer.input(data)
+        res = []
+
+        while True:
+             tok = self.lexer.token()
+             if not tok:
+                 break
+             res.append((tok.type, tok.value))
+        return res
+
+    def assertTokens(self, data, tokens):
+        self.assertEqual(self.tokens(data), tokens)
+
+    def test_keywords(self):
+        self.assertTokens('tempo const voz repetir nota silencio', [('TEMPO', 'tempo'), ('CONST', 'const'), ('VOZ', 'voz'), ('REPETIR', 'repetir'), ('NOTA', 'nota'), ('SILENCIO', 'silencio')])
+
+
+    def test_directives(self):
+        self.assertTokens('#tempo redonda 60', [('HASH', '#'), ('TEMPO', 'tempo'), ('NOTE_VALUE', 'redonda'), ('NUMBER', 60)])
+        self.assertTokens('#compas 3/4', [('HASH', '#'), ('COMPAS', 'compas'), ('NUMBER', 3), ('SLASH', '/'), ('NUMBER', 4)])
+
+
+    def test_const_declaration(self):
+        self.assertTokens('const oct = 5;', [('CONST', 'const'), ('ID', 'oct'), ('EQUALS', '='), ('NUMBER', 5), ('SEMICOLON', ';')])
+
+    def test_ignored_chars(self):
+        self.assertTokens('\tconst  octava=8 ;', [('CONST', 'const'), ('ID', 'octava'), ('EQUALS', '='), ('NUMBER', 8), ('SEMICOLON', ';')])
+
+    def test_comments(self):
+        notas = """
+            nota(sol, 5, blanca);
+            // Este es un comentario entre las dos notas
+            nota(la, 5, negra); // Este tambien vale
+        """
+
+        self.assertTokens(notas, [('NOTA', 'nota'), ('LPAREN', '('), ('MUSICAL_NOTE', 'sol'), ('COMMA', ','), ('NUMBER', 5), ('COMMA', ','), ('NOTE_VALUE', 'blanca'), ('RPAREN', ')'), ('SEMICOLON', ';'),
+            ('NOTA', 'nota'), ('LPAREN', '('), ('MUSICAL_NOTE', 'la'), ('COMMA', ','), ('NUMBER', 5), ('COMMA', ','), ('NOTE_VALUE', 'negra'), ('RPAREN', ')'), ('SEMICOLON', ';')])
+
+    def test_compas_and_notas(self):
+        notas = """
+            compas
+            {
+                nota(sol-, octava, fusa);
+                silencio(corchea.);
+                nota(sol+ , octava, semifusa );
+            }
+        """
+        self.assertTokens(notas, [('COMPAS', 'compas'), ('LBRACKET','{'),
+            ('NOTA', 'nota'), ('LPAREN', '('), ('MUSICAL_NOTE', 'sol'), ('MINUS', '-'), ('COMMA', ','), ('ID', 'octava'), ('COMMA', ','), ('NOTE_VALUE', 'fusa'), ('RPAREN', ')'), ('SEMICOLON', ';'),
+            ('SILENCIO', 'silencio'), ('LPAREN', '('), ('NOTE_VALUE', 'corchea'), ('POINT', '.'), ('RPAREN', ')'), ('SEMICOLON', ';'),
+            ('NOTA', 'nota'), ('LPAREN', '('), ('MUSICAL_NOTE', 'sol'), ('PLUS', '+'), ('COMMA', ','), ('ID', 'octava'), ('COMMA', ','), ('NOTE_VALUE', 'semifusa'), ('RPAREN', ')'), ('SEMICOLON', ';'),
+            ('RBRACKET','}')])
+
+
+if __name__ == '__main__':
+    unittest.main()
